e606cafb257a8e0b3b18b997332adb705d7079af
Saiprakash7337 <124245331+Saiprakash7337@users.noreply.github.com>
2023-02-24 10:13:50 +0530

    Create Tester.java

---
+++ LinkedList2/Tester.java
@@ -0,0 +1,52 @@
+package LinkedList2;
+
+
+public class Tester {
+    public static void main(String[] args) {
+        // Test case 1
+        LinkedList listOne = new LinkedList();
+        listOne.append(10);
+        listOne.append(12);
+        listOne.append(21);
+        listOne.append(1);
+        listOne.append(53);
+        
+        LinkedList listTwo = new LinkedList();
+        listTwo.append(11);
+        listTwo.append(21);
+        listTwo.append(25);
+        listTwo.append(53);
+        listTwo.append(47);
+        
+        LinkedList result = LinkedList.findCommonElements(listOne, listTwo);
+        LinkedListNode current = result.head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+        
+        // Test case 2
+        listOne = new LinkedList();
+        listOne.append(51);
+        listOne.append(45);
+        listOne.append(63);
+        listOne.append(15);
+        listOne.append(82);
+        
+        listTwo = new LinkedList();
+        listTwo.append(19);
+        listTwo.append(63);
+        listTwo.append(51);
+        listTwo.append(87);
+        listTwo.append(82);
+        
+        result = LinkedList.findCommonElements(listOne, listTwo);
+        current = result.head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("");
+    }
+}
